0ab5208e0bb59515c5ed32709080074c6fad9dae
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-12 16:22:34 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
- speed < 40 || speed > 60 ? true : false
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+	end
+ 
 end
 
 def not_safe?(speed)
-		if speed < 40 || speed > 60
-			return true
-		else
-			return false
-		end
+	speed < 40 || speed > 60 ? true : false
 end
